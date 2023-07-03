1abfaa9bfa2d87d4ce4bbe2b9d6609d5146d66d0
AliAtaollahi <aliataollai40@gmail.com>
2023-07-04 02:55:03 +0330

    Add files

--- CA4 - Multi-Cycle/codes/Mux.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns/1ns
-module Mux#(parameter N = 32)(sel,in1,in2,out);
-    input sel;
-    input [N-1:0]in1,in2;
-    output [N-1:0]out;
-
-    assign out=(sel==1'b0)?in1:in2;
-
-endmodule
--- CA4 - Multi-Cycle/codes/Sign_ext.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns/1ns// changed
-module Sign_ext(in,out);
-    input [11:0]in;// 12 bit
-    output [15:0]out;// 16 bit
-    assign out={{4{in[11]}},in};
-
-endmodule
